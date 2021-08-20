5f3e11f2620dcf7989a70dbf8b09026e58bd538b
Facundo García Martoni <thunderxforce@gmail.com>
2021-08-20 18:19:15 -0300

    users_signup_docstring

--- main.py
+++ main.py
@@ -41,6 +41,13 @@ class User(UserBase):
     )
     birth_date: Optional[date] = Field(default=None)
 
+class UserRegister(User):
+    password: str = Field(
+        ..., 
+        min_length=8,
+        max_length=64
+    )
+
 class Tweet(BaseModel):
     tweet_id: UUID = Field(...)
     content: str = Field(
@@ -65,7 +72,22 @@ class Tweet(BaseModel):
     tags=["Users"]
 )
 def signup(): 
-    pass
+    """
+    Signup
+
+    This path operation register a user in the app
+
+    Parameters: 
+        - Request body parameter
+            - user: UserRegister
+    
+    Returns a json with the basic user information: 
+        - user_id: UUID
+        - email: Emailstr
+        - first_name: str
+        - last_name: str
+        - birth_date: str
+    """
 
 ### Login a user
 @app.post(
